01e633d744d17ae60c8ae029ad20cd8c4d419701
wpickup <wpickup@gmail.com>
2024-06-07 15:52:42 +1000

    initial template

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "Surfside Ceramics",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, generated sitemap, and RSS feed.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "Thte internet home of Lee Pickup ceramics.",
+  authorName: "William Pickup",
+  twitterUsername: "", // no `@`
 };
